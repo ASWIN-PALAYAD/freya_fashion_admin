8d39b06cc3b6141a3ec8568d2c8716df24c36619
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-06-21 17:22:52 +0530

    create new product & prodouct page completed

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { columns } from "@/components/collections/CollectionColumn";
 import { DataTable } from "@/components/custom ui/DataTable";
@@ -6,49 +6,48 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const Collections = () => {
-
   const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [collections, setCollections] = useState([]);
 
-  const getCollections = async() => {
+  const getCollections = async () => {
     try {
-      const res = await fetch('/api/collections',{
-        method:"GET"
+      const res = await fetch("/api/collections", {
+        method: "GET",
       });
 
       const data = await res.json();
       setCollections(data);
-      setLoading(false)
-
+      setLoading(false);
     } catch (error) {
       setLoading(false);
-      console.log('[collections_GET',error);
-      
+      console.log("[collections_GET", error);
     }
-  }
+  };
 
   useEffect(() => {
-   getCollections();
-  }, [])
-  
+    getCollections();
+  }, []);
 
   return (
     <div className="px-10 py-5">
-    <div className="flex items-center justify-between">
-      <p className="text-heading2-bold">Collections</p>
-      <Button className="bg-blue-1 text-white" onClick={()=>router.push('/collections/new')} >
-        <Plus className="h-4 w-4 mr-2"/>
-        Create Collection
-      </Button>
+      <div className="flex items-center justify-between">
+        <p className="text-heading2-bold">Collections</p>
+        <Button
+          className="bg-blue-1 text-white"
+          onClick={() => router.push("/collections/new")}
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Create Collection
+        </Button>
+      </div>
+      <Separator className="my-4 bg-grey-1" />
+      <DataTable columns={columns} data={collections} searchKey="title" />
     </div>
-    <Separator className="my-4 bg-grey-1"/>
-      <DataTable columns={columns} data={collections} searchKey = 'title' />
-    </div>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,27 +3,63 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { connectDb } from "@/lib/mongoDB";
 import Product from "@/lib/models/Product";
+import Collection from "@/lib/models/Collection";
 
 //create product
-export const POST = async (req:NextRequest) => {
-    try {
-        const {userId} = auth();
-        if(!userId){
-            return new NextResponse("Unauthorized",{status:401});
-        }
-         await connectDb();
-        const {title,description,media,category,collections,tags,sizes,colors,price,expense} = await req.json();
-        if(!title || !description || !media || !category || !price || !expense){
-            return new NextResponse('Not enough data to create a product',{status:400});
-        }
-        const newProducct = await Product.create({
-            title,description,media,category,collections,tags,sizes,colors,price,expense
-        });
-        await newProducct.save();
-        return NextResponse.json(newProducct,{status:200})
-    } catch (error) {
-        console.log('[products_post]',error);
-        return new NextResponse("Internal error",{status:500})
-        
+export const POST = async (req: NextRequest) => {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-}
+    await connectDb();
+    const {
+      title,
+      description,
+      media,
+      category,
+      collections,
+      tags,
+      sizes,
+      colors,
+      price,
+      expense,
+    } = await req.json();
+    if (!title || !description || !media || !category || !price || !expense) {
+      return new NextResponse("Not enough data to create a product", {
+        status: 400,
+      });
+    }
+    const newProducct = await Product.create({
+      title,
+      description,
+      media,
+      category,
+      collections,
+      tags,
+      sizes,
+      colors,
+      price,
+      expense,
+    });
+    await newProducct.save();
+    return NextResponse.json(newProducct, { status: 200 });
+  } catch (error) {
+    console.log("[products_post]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
+
+//get all products
+export const GET = async (req: NextRequest) => {
+  try {
+    await connectDb();
+    const products = await Product.find()
+      .sort({ createdAt: "desc" })
+      .populate({ path: "collections", model: Collection });
+      return NextResponse.json(products,{status:200});
+  } catch (error) {
+    console.log("[products_get]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+};
